e84cab6a4a4af3b581c41d3591b45afc9305633f
Arevik Hayrapetyan <arevhayrapetyan88@gmail.com>
2021-11-05 15:22:44 +0400

    feat: Added form part

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <List class="list" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import List from "./components/List.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    List,
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.list {
+  display: flex;
+  justify-content: center;
 }
 </style>
---
+++ src/components/List.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="wrapper">
+    <div class="container">
+      <form class="todo-form">
+        <input
+          type="text"
+          placeholder="todo..."
+          class="text-input"
+          v-model="inputValue"
+        />
+        <input type="submit" value="Add" class="add-button" />
+      </form>
+      <div class="added-list">{{ inputValue }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "List",
+  props: {},
+  data() {
+    return {
+      inputValue: "",
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.wrapper {
+  display: flex;
+  justify-content: center;
+}
+.container {
+  width: 300px;
+  height: 300px;
+  display: flex;
+  flex-wrap: wrap;
+}
+.todo-form {
+  width: 300px;
+  display: flex;
+  justify-content: center;
+
+  background-color: #b0b0b0;
+}
+.added-list {
+  background-color: red;
+  height: 50px;
+  display: flex;
+  align-items: center;
+}
+.add-button {
+  height: 35px;
+  width: 100px;
+  color: white;
+  background-color: blue;
+}
+.text-input {
+  height: 30px;
+  width: 400px;
+}
+</style>
